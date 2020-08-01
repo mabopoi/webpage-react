dac2ea6c1518e2ff2a71d42713a7ea7712c26ff6
unknown <mabopoi@gmail.com>
2020-08-01 05:16:02 -0300

    First components. Problem with webpack

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => (
+   <div className="footer">
+       <h4> Would you like to suggest some changes? DM me through my social media!</h4>
+       <a className="footer-link" href="">
+         <img src="" alt="twitter icon" className="footer__icon"/>
+       </a>
+       <a className="footer-link" href="">
+         <img src="" alt="Instragram icon" className="footer__icon"/>
+       </a>
+       <a  className="footer-link"href="">
+         <img src="" alt="Github icon" className="footer__icon"/>
+       </a>
+   </div> 
+)
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+    <div className="header">
+        <img className="header__image--lichess" src="" alt="Lichess logo"/>
+        <h3> Chess page made with ReactJS </h3>
+        <img src="" alt="ReactJS icon" className="header__image--react"/>
+    </div>
+);
+
+export default Header;
